7b661050fa2d5b730736d22a45e2f3fcbf49f39f
shibazzaki <failikyt400@gmail.com>
2024-11-16 18:32:39 +0200

    lab6_wtfform part2

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template
-from .posts import post_bp
-from .users import bp as user_bp
-import os
 
-app = Flask(__name__)
-app.config.from_pyfile("../config.py")
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+def create_app(config_name="config"):
+    app = Flask(__name__)
+    app.config.from_object(config_name)  # Налаштування з конфігурації
 
-app.register_blueprint(post_bp)
-app.register_blueprint(user_bp, url_prefix="/users")
+    # Реєстрація Blueprint'ів
+    from .posts import post_bp
+    from .users import bp as user_bp
+    app.register_blueprint(post_bp)
+    app.register_blueprint(user_bp, url_prefix="/users")
 
-# Імпорт views після створення app
-from . import views
+    # Імпорт views
+    with app.app_context():
+        from . import views
 
+    # Обробник помилки 404
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html'), 404
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
+    return app
--- app/views.py
+++ app/views.py
@@ -1,13 +1,11 @@
-from flask import request, redirect, url_for, render_template, abort
-from . import app
+from flask import request, render_template, current_app
 
-@app.route('/')
+@current_app.route('/')
 def main():
     return render_template("resume.html")
 
-@app.route('/homepage')
+@current_app.route('/homepage')
 def home():
     """View for the Home page of your website."""
     agent = request.user_agent
-
     return render_template("home.html", agent=agent)
